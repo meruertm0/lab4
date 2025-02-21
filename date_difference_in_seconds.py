4f0c49de6302cdde53461797e9811e95fc960008
meruertm0 <m_mukhtarkyzy@kbtu.kz>
2025-02-22 00:51:16 +0500

    Update date_difference_in_seconds.py

--- date_difference_in_seconds.py
+++ date_difference_in_seconds.py
@@ -1,10 +1,8 @@
 from datetime import datetime
 
-# Define two dates
 date1 = datetime(2025, 2, 20, 14, 30, 0)
 date2 = datetime(2025, 2, 21, 16, 45, 0)
 
-# Calculate difference in seconds
 difference_in_seconds = (date2 - date1).total_seconds()
 
 print("Difference in Seconds:", difference_in_seconds)
